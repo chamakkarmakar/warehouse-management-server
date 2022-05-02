9ca0ac91c0112ab3543d0e985c9c2b595aa48f89
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-05-02 23:24:51 +0600

    load data from mongodb

--- index.js
+++ index.js
@@ -16,20 +16,32 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dpqbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('warehouse management database connected');
-  // perform actions on the collection object
-  client.close();
-});
+console.log('warehouse management database connected');
+async function run() {
+    try {
+        await client.connect();
+        const inventoryCollection = client.db('warehouseManagement').collection('inventory');
+
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 // root API 
-app.get ('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Warehouse Management Server Side');
 })
 
 // listening to port 
-app.listen(port,()=>{
-    console.log('Running Port',port);
+app.listen(port, () => {
+    console.log('Running Port', port);
 })
